abab23bb58c005c61037b087db8b6d7063e42d57
Jonas Magalhães <jonasevmagalhaes@gmail.com>
2024-12-27 14:13:04 -0300

    Criado projeto com flyway

---
+++ src/test/java/com/potatosoft/life/LifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatosoft.life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
